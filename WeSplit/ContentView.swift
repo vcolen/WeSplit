a0cfce43ef4c51953f66b0974391bb31ba256dcf
VIctor <colen.victor@icloud.com>
2021-11-18 14:09:50 -0300

    part 2 complete

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 1
+    @State private var tipPercentage = 10
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [0, 5, 10, 15, 20]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 1)
+        let selectedTip = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * selectedTip
+        
+        return (checkAmount + tipValue) / peopleCount
+    }
+    
     var body: some View {
-        Text("Hello World")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "BRL") )
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                } header: {
+                    Text("Check amount")
+                }
+                
+                Section {
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(1..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Tip amount")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "BRL"))
+                } header: {
+                    Text("Total")
+                }
+                .navigationTitle("WeSplit")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Button("Done") {
+                            amountIsFocused = false
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
